0ca02d0f91b5ff02496fb259d667c16b5b2305cc
Ajith PM <Ajith@appentus.in>
2025-08-09 22:26:07 +0530

    scope

--- 02-variables-scopes/scope.js
+++ 02-variables-scopes/scope.js
@@ -1,2 +1,27 @@
-var hi = 2;
-console.log(hi);
+// Scope in JavsScript refers to the visibility of varibles and functions within a program. In JavaScript there is three types of scope Glopal Scope, Function Scope and Block Scope
+
+// Here variable x is declared in the Global Scope, so it can be accessed from anywhere in the program
+var x = "Welcome to JavaScript"
+
+function example() {
+    console.log(x);
+}
+example();
+
+console.log(x);
+
+// Function Scope
+function sayHello() {
+    let a = "Hello";
+    console.log(a);
+}
+sayHello();
+
+// Block Scope
+function hey() {
+    if (true) {
+        let y = 6;
+        console.log(y);
+    }
+}
+hey();
