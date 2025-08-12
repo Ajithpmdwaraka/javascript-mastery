aa8a0587ab7ec2f7198dc3d94ccd5204830e38a4
Ajith PM <Ajith@appentus.in>
2025-08-12 19:38:15 +0530

    comparison

---
+++ 04-operators/comparison.js
@@ -0,0 +1,16 @@
+// Comparison operators are used to compare two values and return a boolean value
+
+let a = 20;
+let b = 40;
+
+console.log(a > b);
+console.log(a < b);
+console.log(a >= b);
+console.log(a <= b);
+console.log(a == b); // Loose equality (type coercion)
+console.log(a === b); // Strict equality (no type coercion)
+console.log(a != b); // Loose inequality (type coercion)
+console.log(a !== b); // Strict inequality (no type coercion)
+console.log(a > b && a < b); // Logical AND
+console.log(a > b || a < b); // Logical OR
+console.log(!(a > b)); // Logical NOT
