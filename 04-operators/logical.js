81981e7689bf064715ba39d05ee2d9cca88c0cd0
Ajith PM <Ajith@appentus.in>
2025-08-12 21:39:03 +0530

    logical operators

---
+++ 04-operators/logical.js
@@ -0,0 +1,20 @@
+// Logical Operators are used to perform logical operations, the logical operators are AND &&, OR ||, NOT !
+
+let a = 4;
+let b = 6;
+
+console.log(a > 0 && b > 0);
+console.log(a > 0 && b < 2);
+console.log(a < 0 && b > 2);
+console.log(a < 0 && b < 8);
+
+console.log(a > 0 || b > 0);
+console.log(a > 0 || b < 2);
+console.log(a < 0 || b > 2);
+console.log(a < 0 || b < 8);
+
+let yes = true;
+let no = false;
+
+console.log(!yes);
+console.log(!no);
