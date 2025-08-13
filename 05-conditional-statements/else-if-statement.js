b4f841e2a9307849e26d36f74ce4a6e76cf36b4f
Ajith PM <Ajith@appentus.in>
2025-08-13 11:25:33 +0530

    else-if-statement

---
+++ 05-conditional-statements/else-if-statement.js
@@ -0,0 +1,11 @@
+// Else If Statement
+
+let age = 16;
+
+if (age >= 18) {
+    console.log("You are an Adult");
+} else if (age>= 16) {
+    console.log("You are a teenager");
+} else {
+    console.log("You are a child");
+}
