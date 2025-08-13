f68c689f8621a7e3e2f097b13344f3922d4db325
Ajith PM <Ajith@appentus.in>
2025-08-13 11:21:03 +0530

    else statement

---
+++ 05-conditional-statements/if-else-statement.js
@@ -0,0 +1,9 @@
+// If-Else Statement in JavaScript is used to execute a block of code it the condition is true and an another block of code if the condition is false
+
+let age = 20;
+
+if (age > 18) {
+    console.log("You are eligible for license");
+} else {
+    console.log("You are not eligible for license");
+}
