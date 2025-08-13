11316186907f94df23517a35b2680435c0ef41b5
Ajith PM <Ajith@appentus.in>
2025-08-13 11:16:59 +0530

    If statement

---
+++ 05-conditional-statements/if-statement.js
@@ -0,0 +1,15 @@
+// If statement is used to execute a block of code if the condition is true
+
+let a = 20;
+
+if (a > 16) {
+    console.log(a, " is greater than 16 ");
+};
+
+
+let age = 18;
+let country = "India";
+
+if (age >= 18 && country =="India"){
+    console.log("You are eligible for license");
+};
