410b23cddec9dbe0c60f3353ca5b116e330064b8
Ajith PM <ajithpmdwaraka@gmail.com>
2025-08-16 18:28:09 +0530

    anonymous function

---
+++ 07-functions/anonymous-function.js
@@ -0,0 +1,14 @@
+let sum = function (x, y) {
+    return x + y;
+}
+
+console.log(sum(2, 4));
+
+(function () {
+    console.log("Welcome to kerala")
+}
+)();
+
+setTimeout(function () {
+    console.log("Hello Fellas")
+}, 2000);
